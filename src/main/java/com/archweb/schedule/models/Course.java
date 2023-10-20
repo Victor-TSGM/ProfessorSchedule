6be36fc90c21e8f378c592d615f1c12f07cf089f
Victor Martins <victor.martins00@outlook.com>
2023-10-19 22:06:33 -0300

    feat: generate getters, setters and constructor for Course

--- src/main/java/com/archweb/schedule/models/Course.java
+++ src/main/java/com/archweb/schedule/models/Course.java
@@ -36,4 +36,111 @@ public class Course {
     @ManyToMany(mappedBy = "courses")
     private List<Professor> professors;
 
+    public Course(Integer id, String description, int workload, String objectives, String menus) {
+        this.id = id;
+        this.description = description;
+        this.workload = workload;
+        this.objectives = objectives;
+        this.menus = menus;
+    }
+
+
+    /**
+     * @return Integer return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return int return the workload
+     */
+    public int getWorkload() {
+        return workload;
+    }
+
+    /**
+     * @param workload the workload to set
+     */
+    public void setWorkload(int workload) {
+        this.workload = workload;
+    }
+
+    /**
+     * @return String return the objectives
+     */
+    public String getObjectives() {
+        return objectives;
+    }
+
+    /**
+     * @param objectives the objectives to set
+     */
+    public void setObjectives(String objectives) {
+        this.objectives = objectives;
+    }
+
+    /**
+     * @return String return the menus
+     */
+    public String getMenus() {
+        return menus;
+    }
+
+    /**
+     * @param menus the menus to set
+     */
+    public void setMenus(String menus) {
+        this.menus = menus;
+    }
+
+    /**
+     * @return List<Schedule> return the schedules
+     */
+    public List<Schedule> getSchedules() {
+        return schedules;
+    }
+
+    /**
+     * @param schedules the schedules to set
+     */
+    public void setSchedules(List<Schedule> schedules) {
+        this.schedules = schedules;
+    }
+
+    /**
+     * @return List<Professor> return the professors
+     */
+    public List<Professor> getProfessors() {
+        return professors;
+    }
+
+    /**
+     * @param professors the professors to set
+     */
+    public void setProfessors(List<Professor> professors) {
+        this.professors = professors;
+    }
+
 }
