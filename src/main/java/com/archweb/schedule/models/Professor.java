414f5e2604f57512fd1f6ba638a83238689703b1
Victor Martins <victor.martins00@outlook.com>
2023-10-19 22:06:42 -0300

    feat: generate getters, setters and constructor for Professor

--- src/main/java/com/archweb/schedule/models/Professor.java
+++ src/main/java/com/archweb/schedule/models/Professor.java
@@ -34,7 +34,100 @@ public class Professor {
     @ManyToOne
     @JoinColumn(name = "address_id")
     private Address address;
-    @OneToMany(mappedBy = "course")
+    @ManyToMany(mappedBy = "course")
     private List<Schedule> schedules; // rua josé, 200 - sorocaba -sao paulo 18000000
+
+    public Professor(String name, String cpf, String rg, String phone, Address address) {
+        this.name = name;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.phone = phone;
+        this.address = address;
+    }
+    
+
+    /**
+     * @return Integer return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return String return the rg
+     */
+    public String getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return String return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return Address return the address
+     */
+    public Address getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
 }
-// @ManyToMany
