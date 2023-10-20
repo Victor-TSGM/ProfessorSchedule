d90db794957355926123a5e33cfec723b7200034
Victor Martins <victor.martins00@outlook.com>
2023-10-19 22:06:23 -0300

    feat: generate getters, setters and constructor for Address

--- src/main/java/com/archweb/schedule/models/Address.java
+++ src/main/java/com/archweb/schedule/models/Address.java
@@ -29,4 +29,114 @@ public class Address {
     private int number;
     @Column(nullable = false)
     private int zipCode;
+
+    
+    
+    public Address(String country, String state, String city, String street, int number, int zipCode) {
+        this.country = country;
+        this.state = state;
+        this.city = city;
+        this.street = street;
+        this.number = number;
+        this.zipCode = zipCode;
+    }
+
+    /**
+     * @return Integer return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return String return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * @return String return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return String return the street
+     */
+    public String getStreet() {
+        return street;
+    }
+
+    /**
+     * @param street the street to set
+     */
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    /**
+     * @return int return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    /**
+     * @return int return the zipCode
+     */
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    /**
+     * @param zipCode the zipCode to set
+     */
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
 }
